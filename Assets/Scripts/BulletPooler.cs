ea8816ef10323b7550e8dc6fe3966c55113a563b
Owen Engle <owen.engle@chatham.edu>
2020-02-12 18:21:35 -0500

    Updates to bullet pooler
    Basic functionality added(i hope). Checks the list for bullets, if there are none, creates one and adds to list. If there is one, sets it active

--- Assets/Scripts/BulletPooler.cs
+++ Assets/Scripts/BulletPooler.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class BulletPooler : MonoBehaviour
 {
     List<GameObject> pool;
+    public GameObject bulletprefab;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        pool = new List<GameObject>();
+        GameObject bullet = GameObject.Instantiate(bulletprefab);
 
+        if (pool.Contains(bullet))
+        {
+            bullet.SetActive(true);
+        }
+        else
+        {
+            GameObject.Instantiate(bulletprefab);
+            pool.Add(bulletprefab);
+        }
     }
 
     // Update is called once per frame
