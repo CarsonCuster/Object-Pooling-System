1e732466c6716c1dfda3728299ea94ac33f0f3d4
CarsonCuster <carsonwcuster@gmail.com>
2020-02-12 19:24:31 -0500

    Added and updated WeaponManager script

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [Header("Settings")]
+    public GameObject bullet;
+    float timer = 0;
+
+
+    public float secondsbeforedisable;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer = timer+Time.deltaTime;
+        if(Input.GetButton("Jump"))
+        {
+            GameObject bullet = BulletPooler.instance.GetPooledObject();
+            if (bullet != null)
+            {
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+                bullet.SetActive(true);
+                bullet.GetComponent<Rigidbody2D>();
+            }
+        }
+
+        if(timer > secondsbeforedisable)
+        {
+            bullet.SetActive(false);
+        }
+
+
+    }
+}
